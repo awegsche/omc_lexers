2e15a7e94aff24130e29aacc3fdd828994d4c895
awegsche <a.wegscheider7141@gmail.com>
2021-09-08 15:47:54 +0200

    ups, that was just a symlink

--- mad.py
+++ mad.py
@@ -1,1 +1,16 @@
-../pylhc.github.io/scripts/madlexer.py
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+class CustomLexer(RegexLexer):
+    name = "MadX"
+    aliases = ['mad', 'madx']
+    filenames = ['*.mad', '*.seq']
+
+    tokens = {
+        'root': [
+            ('TWISS', Name.Class),
+            (r'\btwiss,.*;', Name.Class),
+            (r'!.*$', Comment),
+            (r'//.*$', Comment),
+        ]
+    }
