3f5f035f804a7c256707daf7ba98ecc3ed09a389
awegsche <a.wegscheider7141@gmail.com>
2021-09-08 17:34:12 +0200

    added some serious keywords

--- omc_lexers/mad.py
+++ omc_lexers/mad.py
@@ -1,16 +1,60 @@
-from pygments.lexer import RegexLexer
+from pygments.lexer import RegexLexer, bygroups, include
 from pygments.token import *
 
+MAD_CMD_LIST = [
+    "twiss",
+    "call",
+    "use",
+    "option",
+    "beam",
+    "title",
+]
+
+MAD_PROP_LIST = [
+    "sequence",
+    "file",
+    "particle",
+    "energy",
+    "kbunch",
+    "npart",
+    "bv",
+]
 class MadLexer(RegexLexer):
     name = "MadX"
     aliases = ['mad', 'madx']
     filenames = ['*.mad', '*.seq']
 
     tokens = {
+        'strings': [
+            (r"'.*'", String),
+            (r'".*"', String),
+        ],
         'root': [
-            ('TWISS', Name.Class),
-            (r'\btwiss,.*;', Name.Class),
+            include('strings'),
+            (f'\\b({"|".join(MAD_CMD_LIST)})\\b', Name.Class, 'twiss'),
+            (f'\\b({"|".join([x.upper() for x in MAD_CMD_LIST])})\\b', Name.Class, 'twiss'),
             (r'!.*$', Comment),
             (r'//.*$', Comment),
+            ("\\bexec", Name.Class, "exec"),
+            ("\\b(if|else)", Keyword),
+            (r'/\*', Comment.Multiline, 'comment'),
+        ],
+         'comment': [
+            (r'[^*/]', Comment.Multiline),
+            (r'/\*', Comment.Multiline, '#push'),
+            (r'\*/', Comment.Multiline, '#pop'),
+            (r'[*/]', Comment.Multiline)
+        ],
+        'twiss': [
+            include('strings'),
+            (r';', Text, '#pop'),
+            (f'\\b({"|".join(MAD_PROP_LIST)})', Name.Attribute),
+            (f'\\b({"|".join([x.upper() for x in MAD_PROP_LIST])})', Name.Attribute),
+            (r'[^;]', Text),
+        ],
+        'exec': [
+            ('\\w+', Name.Function),
+            ('\\(', Text, "#pop"),
         ]
+        
     }
